73a99116feafe6b30d8aed65309cbf3eb3ec2a2a
CrisSotomayor <cristina@sotomayor.com.mx>
2021-03-26 11:35:22 -0600

    Se añadió el archivo template.py

---
+++ templates/template.py
@@ -0,0 +1,28 @@
+'''
+##  Name: [Name]
+##  Version: [#.#]
+
+##  Author:
+        Cristina Sotomayor <cristina@lcg.unam.mx>
+
+##  Created: [YYYY-MM-DD]
+
+##  Description:
+        
+
+##  Usage:
+        program_name [options list] arguments
+
+##  Arguments:
+        [name] description
+
+##  Requirements:
+        [libraries]
+
+##  Input:
+        [files]
+
+## Output:
+        [files]
+
+'''
